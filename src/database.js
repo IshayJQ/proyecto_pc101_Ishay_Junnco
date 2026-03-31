bff2ea9afda21e6c855da7a949b03b3459334682
Ishay Junnco <113075680+IshayJQ@users.noreply.github.com>
2026-03-30 19:56:25 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -10,7 +10,6 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3000
 });
 
 pool.getConnection((err, connection) => {
