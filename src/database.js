b08e2264cf636355bfae43cee4664e9e9788387a
Ishay Junnco <113075680+IshayJQ@users.noreply.github.com>
2026-03-30 19:23:18 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,7 +3,15 @@ const { promisify }= require('util');
 
 const { database } = require('./keys');
 
-const pool = mysql.createPool(database);
+//const pool = mysql.createPool(database);
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3000
+});
 
 pool.getConnection((err, connection) => {
   if (err) {
